d05d83d1b4a6f4387c390d6fdfbfd340fc717095
Mayumi <datadrivepribadi@gmail.com>
2025-06-19 01:52:24 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,10 @@ use tokio_native_tls::TlsConnector as TokioTlsConnector; // Konektor TLS async
 
 const IP_RESOLVER: &str = "speed.cloudflare.com";
 const PATH_RESOLVER: &str = "/meta";
-const PROXY_FILE: &str = "Data/hasil.txt";
-const OUTPUT_FILE: &str = "Data/alive/hasil.txt";
-const MAX_CONCURRENT: usize = 50;
-const TIMEOUT_SECONDS: u64 = 10;
+const PROXY_FILE: &str = "Data/IPProxy19-6.txt"; //input
+const OUTPUT_FILE: &str = "Data/hidup/IPProxy19-6.txt";
+const MAX_CONCURRENT: usize = 100;
+const TIMEOUT_SECONDS: u64 = 5;
 
 // Define a custom error type that implements Send + Sync
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
